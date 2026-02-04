5c16799c145e2130fac47dd683e0d4b6f99939cc
moghost88 <acmt88cjr@gmail.com>
2026-02-04 19:52:41 +0200

    Fix blank screen by removing redundant routers in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
-import { LanguageProvider } from "./contexts/LanguageContext.tsx";
 import "./index.css";
 
 console.log("App initializing...");
 
 createRoot(document.getElementById("root")!).render(
-    <LanguageProvider>
-        <HashRouter>
-            <App />
-        </HashRouter>
-    </LanguageProvider>
+    <App />
 );
